d1c5fdf9a4e0f81a230ffcaf557c4b05eb303d7e
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-01-21 02:00:16 +0700

    Searching Message

--- Assets/ARTag/Scripts/Environment/EnvironmentController.cs
+++ Assets/ARTag/Scripts/Environment/EnvironmentController.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 using GoogleARCore;
 using GoogleARCore.HelloAR;
 
-public class EnvironmentController : MonoBehaviour {
+public class EnvironmentController : Publisher
+{
 
     public Camera firstPersonCamera;
+
     public GameObject trackedPlanePrefab;
 
     List<TrackedPlane> newPlanes;
     List<TrackedPlane> allPlanes;
+
+    bool previousSearchingStatus;
+
+    const int SLEEP_TRACKING_TIMEOUT = 15;
 
     void Awake()
     {
@@ -20,9 +26,19 @@ public class EnvironmentController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        EnvironmentUpdate();
+        if (IsTracking())
+        {
+            PreventSleep();
+            EnvironmentUpdate();
+            CheckSearchingPlanesStatus();
+        }
+        else
+        {
+            SetLostTrackingTimeout();
+        }
     }
 
+#region Plane Construct
     void GetNewPlanes()
     {
         Frame.GetPlanes(newPlanes, TrackableQueryFilter.New);
@@ -42,4 +58,54 @@ public class EnvironmentController : MonoBehaviour {
             CreateNewPlane(trackedPlane);
         }
     }
+    #endregion
+
+#region Tracking Status
+    bool IsTracking()
+    {
+        return Frame.TrackingState == TrackingState.Tracking;
+    }
+
+    void SetLostTrackingTimeout()
+    {
+        Screen.sleepTimeout = SLEEP_TRACKING_TIMEOUT;
+    }
+
+    void PreventSleep()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
+    #endregion
+
+#region Plane Searching Status
+    bool IsPlaneInTrackingState(TrackedPlane plane)
+    {
+        return plane.TrackingState == TrackingState.Tracking;
+    }
+
+    void UpdateNewSearchingStatus(bool newSearchingStatus)
+    {
+        Broadcast("OnSearchingStateChange", newSearchingStatus);
+        previousSearchingStatus = newSearchingStatus;
+    }
+
+    void CheckSearchingPlanesStatus()
+    {
+        Frame.GetPlanes(allPlanes);
+        bool isSearching = true;
+        foreach (TrackedPlane trackedPlane in allPlanes)
+        {
+            if (IsPlaneInTrackingState(trackedPlane))
+            {
+                isSearching = false;
+                break;
+            }
+        }
+        if (isSearching != previousSearchingStatus)
+            UpdateNewSearchingStatus(isSearching);
+    }
+#endregion
+
+
+
 }
