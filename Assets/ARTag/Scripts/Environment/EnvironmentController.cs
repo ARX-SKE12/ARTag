2959553a833ba666db13aaf88415470a87ce37f8
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-01-21 01:07:52 +0700

    Environment Controller Can Create Plane

---
+++ Assets/ARTag/Scripts/Environment/EnvironmentController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+using GoogleARCore.HelloAR;
+
+public class EnvironmentController : MonoBehaviour {
+
+    public Camera firstPersonCamera;
+    public GameObject trackedPlanePrefab;
+
+    List<TrackedPlane> newPlanes;
+    List<TrackedPlane> allPlanes;
+
+    void Awake()
+    {
+        newPlanes = new List<TrackedPlane>();
+        allPlanes = new List<TrackedPlane>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        EnvironmentUpdate();
+    }
+
+    void GetNewPlanes()
+    {
+        Frame.GetPlanes(newPlanes, TrackableQueryFilter.New);
+    }
+
+    void CreateNewPlane(TrackedPlane plane)
+    {
+        GameObject planeObject = Instantiate(trackedPlanePrefab, Vector3.zero, Quaternion.identity, transform);
+        planeObject.GetComponent<TrackedPlaneVisualizer>().Initialize(plane);
+    }
+
+    void EnvironmentUpdate()
+    {
+        GetNewPlanes();
+        foreach (TrackedPlane trackedPlane in newPlanes)
+        {
+            CreateNewPlane(trackedPlane);
+        }
+    }
+}
