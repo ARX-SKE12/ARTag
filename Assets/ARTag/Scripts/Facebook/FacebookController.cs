f4397b3cfae82edebd63dfbbe992a800685ddbb2
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-01-30 14:29:09 +0700

    Init FB SDK

---
+++ Assets/ARTag/Scripts/Facebook/FacebookController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Facebook.Unity;
+
+public class FacebookController : MonoBehaviour {
+
+    void Awake()
+    {
+        InitFacebookSDKConnection();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void InitFacebookSDKConnection()
+    {
+        if (!FB.IsInitialized) FB.Init(OnInitSuccess, OnHideUnity);
+        FB.ActivateApp();
+    }
+
+    void OnInitSuccess()
+    {
+        if (FB.IsInitialized) Debug.Log("Facebook SDK Initialization Successful!");
+        else Debug.Log("Failed to Initialize the Facebook SDK");
+    }
+
+    void OnHideUnity (bool isGameShown)
+    {
+        if (!isGameShown) Time.timeScale = 0;
+        else Time.timeScale = 1;
+    }
+}
