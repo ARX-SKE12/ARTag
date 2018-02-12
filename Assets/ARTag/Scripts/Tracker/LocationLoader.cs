fe750cfd51a6d62dc75afd400daf098033041807
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-02-12 15:38:26 +0700

    Init Some Project

---
+++ Assets/ARTag/Scripts/Tracker/LocationLoader.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Wikitude;
+public class LocationLoader : MonoBehaviour {
+    public GameObject wikitudeObject, arCoreObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnImageRecognized(ImageTarget recognizedTarget)
+    {
+        
+    }
+}
