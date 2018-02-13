4b3cec85d1e8a655c42655dcf0f982b89419b1d5
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-02-13 10:11:31 +0700

    Log Distance

--- Assets/ARTag/Scripts/Tracker/LocationLoader.cs
+++ Assets/ARTag/Scripts/Tracker/LocationLoader.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using Wikitude;
 public class LocationLoader : MonoBehaviour {
-    public GameObject wikitudeObject, arCoreObject;
+    public GameObject wikitudeObject, arCoreObject, refPoint, wikiCam;
+    ImageTarget recognizedTarget = null;
+    bool isTracking;
 	// Use this for initialization
 	void Start () {
         LogClient.instance.Log("Status", "Init");
@@ -11,12 +13,27 @@ public class LocationLoader : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (isTracking)
+        {
+            LogClient.instance.Log("Location Name", recognizedTarget.Name);
+            LogClient.instance.Log("Location ID", recognizedTarget.ID.ToString());
+            LogClient.instance.Log("Distance", recognizedTarget.ComputeCameraDistanceToTarget().ToString());
+            LogClient.instance.Log("Diff", (wikiCam.transform.position - refPoint.transform.position).ToString());
+        }
 	}
 
     public void OnImageRecognized(ImageTarget recognizedTarget)
     {
-        LogClient.instance.Log("Location Name", recognizedTarget.Name);
-        LogClient.instance.Log("Location ID", recognizedTarget.ID.ToString());
+        if (recognizedTarget.ID!=0)
+        {
+            isTracking = true;
+            this.recognizedTarget = recognizedTarget;
+        }
+    }
+
+    public void OnImageLost()
+    {
+        isTracking = false;
+        recognizedTarget = null;
     }
 }
