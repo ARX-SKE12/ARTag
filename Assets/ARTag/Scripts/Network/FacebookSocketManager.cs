457ce9c7cb8129f4c994a3890114bbcf342f3374
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-02-22 22:04:42 +0700

    Auth with Socket IO

---
+++ Assets/ARTag/Scripts/Network/FacebookSocketManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class FacebookSocketManager : MonoBehaviour {
+
+    public GameObject facebookController;
+
+    SocketManager socket;
+
+    const string authURL = "http://localhost:3000/auth/facebook/token";
+
+	// Use this for initialization
+	void Start () {
+        socket = GetComponent<SocketManager>();
+        facebookController.GetComponent<FacebookController>().Register(gameObject);
+	}
+	
+    void OnAuthSuccess(string token)
+    {
+        Dictionary<string, string> data = new Dictionary<string, string>();
+        data["token"] = token;
+        socket.Emit("auth", new JSONObject(data));
+    }
+}
