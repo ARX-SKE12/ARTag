201685a685707488b5bbd3645952b854350daae8
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-03-03 22:01:03 +0700

    Use Logging Client

--- Assets/ARTag/Scripts/Tracker/LocationLoader.cs
+++ Assets/ARTag/Scripts/Tracker/LocationLoader.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using LoggingClient;
 using UnityEngine;
 using Wikitude;
 public class LocationLoader : MonoBehaviour {
---
+++ Assets/LoggingClient/Example/Scripts/ExampleScript.cs
@@ -0,0 +1,10 @@
+﻿using LoggingClient;
+using UnityEngine;
+
+public class ExampleScript : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+        LogClient.instance.Log(LogClient.CLIENT_STATUS_TAG, "Test");		
+	}
+}
