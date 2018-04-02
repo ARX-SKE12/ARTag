a5cff436d25b06786226dc83c0fb5ff645ce3eb2
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-04-02 12:29:33 +0700

    Rebuild Project

---
+++ Assets/ARTag/Scripts/Marker/CloudStatusReporter.cs
@@ -0,0 +1,42 @@
+﻿
+namespace ARTag
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+    using Wikitude;
+
+    public class CloudStatusReporter : MonoBehaviour
+    {
+        double recognitionInterval = 1;
+
+        public void OnInitialized()
+        {
+            GameObject.Find(ObjectsCollector.CLOUD_STATUS_TEXT).GetComponent<Text>().text = "Cloud Initialized!";
+            GetComponent<ImageTracker>().CloudRecognitionService.StartContinuousRecognition(recognitionInterval);
+        }
+
+        public void OnError()
+        {
+            GameObject.Find(ObjectsCollector.CLOUD_STATUS_TEXT).GetComponent<Text>().text = "Cloud Error!";
+        }
+
+        public void OnRecognitionResponse(CloudRecognitionServiceResponse response)
+        {
+            if (response.Recognized)
+            {
+                GameObject.Find(ObjectsCollector.CLOUD_STATUS_TEXT).GetComponent<Text>().text = response.Info["name"];
+            }
+            else
+            {
+                GameObject.Find(ObjectsCollector.CLOUD_STATUS_TEXT).GetComponent<Text>().text = "No target recognized";
+            }
+        }
+
+        public void OnInterruption(double suggestedInterval)
+        {
+            recognitionInterval = suggestedInterval;
+            GetComponent<ImageTracker>().CloudRecognitionService.StartContinuousRecognition(recognitionInterval);
+        }
+    }
+
+}
---
+++ Assets/ARTag/Scripts/ObjectsCollector.cs
@@ -0,0 +1,23 @@
+﻿
+namespace ARTag
+{
+    public class ObjectsCollector
+    {
+        public static readonly string FACEBOOK_AUTH_CONTROLLER_OBJECT = "Facebook Auth Controller";
+        public static readonly string FACEBOOK_STATUS_TEXT = "Status Text";
+        public static readonly string SOCKETIO_MANAGER_OBJECT = "SocketIO Manager";
+        public static readonly string USER_INFO_TEXT = "User Info Text";
+        public static readonly string MARKER_IMAGE = "Marker Image";
+        public static readonly string THUMBNAIL_IMAGE = "Thumbnail Image";
+        public static readonly string MARKER_SIZE_FIELD = "Marker Size InputField";
+        public static readonly string PLACE_NAME_FIELD = "Place Name InputField";
+        public static readonly string PLACE_DESCRIPTION_FIELD = "Place Description InputField";
+        public static readonly string PUBLIC_CHECK_BOX = "Public Use Checkbox";
+        public static readonly string ERROR_TEXT = "Error Text";
+        public static readonly string FORM_STATUS_TEXT = "Form Status Text";
+        public static readonly string CLOUD_STATUS_TEXT = "Cloud Status Text";
+        public static readonly string QUALITY_TRACKING_TEXT = "Quality Tracking Text";
+    }
+
+}
+
