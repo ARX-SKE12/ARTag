83f2412f1f9d835612cd9523671fa7b07e346770
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-04-05 18:06:20 +0700

    Complete QR Scan

---
+++ Assets/ARTag/Scripts/QR/ReadSignificantQR.cs
@@ -0,0 +1,23 @@
+﻿
+namespace ARTag
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+    using JustAQRScanner;
+
+    public class ReadSignificantQR : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+            GameObject.FindObjectOfType<QRReader>().Register(gameObject);
+        }
+
+        void OnQRDetect(string result)
+        {
+            GetComponent<Text>().text = result;
+        }
+    }
+
+}
