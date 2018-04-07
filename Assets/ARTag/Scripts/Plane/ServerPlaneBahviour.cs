90482f9b0faebb92edfb89a697fbf81e241a587d
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-04-07 23:57:58 +0700

    Plane Restoration

---
+++ Assets/ARTag/Scripts/Plane/ServerPlaneBahviour.cs
@@ -0,0 +1,19 @@
+﻿
+namespace ARTag
+{
+    
+    using UnityEngine;
+
+    public class ServerPlaneBahviour : MonoBehaviour
+    {
+
+        public void Initialize(Plane plane)
+        {
+            Mesh mesh = GetComponent<MeshFilter>().mesh;
+            mesh.SetIndices(plane.indices, MeshTopology.Triangles, 0);
+            mesh.SetVertices(plane.vertices);
+
+        }
+    }
+
+}
