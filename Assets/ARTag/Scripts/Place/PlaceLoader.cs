a8c222de256ce3131fcf55b671542287e9058a4a
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-04-14 01:28:40 +0700

    QR Save UI

---
+++ Assets/ARTag/Scripts/Place/PlaceLoader.cs
@@ -0,0 +1,34 @@
+﻿
+namespace ARTag
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class PlaceLoader : MonoBehaviour
+    {
+        public GameObject background;
+        string significant;
+        Place place;
+
+        const string BASE_URL = "https://storage.googleapis.com/artag-thumbnail/";
+
+        // Use this for initialization
+        void Start()
+        {
+            significant = (string)GameObject.FindObjectOfType<TemporaryDataManager>().Get("significant");
+            StartCoroutine(LoadImage());
+        }
+
+        IEnumerator LoadImage()
+        {
+            string url = BASE_URL + significant + ".png";
+            WWW request = new WWW(url);
+            yield return request;
+            Texture2D source = request.texture;
+            background.GetComponent<Image>().sprite = Sprite.Create(source, new Rect(0, 0, source.width, source.height), Vector2.zero);
+        }
+    }
+
+}
