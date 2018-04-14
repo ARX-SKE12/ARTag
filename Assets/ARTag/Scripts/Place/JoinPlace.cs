a7870bccc91510de300ad3e80430a90d465d2e8b
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-04-14 21:32:20 +0700

    Join Editor Mode

---
+++ Assets/ARTag/Scripts/Place/JoinPlace.cs
@@ -0,0 +1,28 @@
+﻿
+namespace ARTag
+{
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+    using SocketIOManager;
+
+    public class JoinPlace : MonoBehaviour
+    {
+
+        SocketManager socketManager;
+
+        // Use this for initialization
+        void Start()
+        {
+            socketManager = GameObject.FindObjectOfType<SocketManager>();
+        }
+
+        public void JoinRoom(string sceneName)
+        {
+            JSONObject data = new JSONObject();
+            data.AddField("placeId", ((Place)GameObject.FindObjectOfType<TemporaryDataManager>().Get("currentPlace")).id);
+            socketManager.Emit(EventsCollector.ROOM_JOIN, data);
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
+}
