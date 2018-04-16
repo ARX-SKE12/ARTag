fa10896ae7a59c7772ba163013d18238d915e2fe
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-04-16 23:54:13 +0700

    Solve Padding bug in Tag FOrm

--- Assets/ARTag/Scripts/Tag/TagFormController.cs
+++ Assets/ARTag/Scripts/Tag/TagFormController.cs
@@ -1,16 +1,59 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿
+namespace ARTag
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
 
-public class TagFormController : MonoBehaviour {
+    public class TagFormController : MonoBehaviour
+    {
+        int type;
+        public GameObject thumbnail, title, range, size, description; 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void SelectType(int type)
+        {
+            switch (type)
+            {
+                case 1:
+                    thumbnail.SetActive(false);
+                    description.SetActive(false);
+                    break;
+                case 2:
+                    thumbnail.SetActive(false);
+                    break;
+                default:
+                    break;
+            }
+            if (!thumbnail.activeSelf)
+            {
+                GetComponent<VerticalLayoutGroup>().padding.left = 40;
+                GetComponent<VerticalLayoutGroup>().padding.right = 40;
+                GetComponent<VerticalLayoutGroup>().padding.top = 40;
+            } else
+            {
+                GetComponent<VerticalLayoutGroup>().padding.left = 0;
+                GetComponent<VerticalLayoutGroup>().padding.right = 0;
+                GetComponent<VerticalLayoutGroup>().padding.top = 0;
+            } 
+        }
+
+        public void Create()
+        {
+
+        }
+    }
+
 }
---
+++ Assets/ARTag/Scripts/Tag/TagTypeSelector.cs
@@ -0,0 +1,20 @@
+﻿
+namespace ARTag
+{
+    using UnityEngine;
+
+    public class TagTypeSelector : MonoBehaviour
+    {
+        public int type;
+        public GameObject selectModal, tagForm;
+        
+        public void ShowForm()
+        {
+            tagForm.SetActive(true);
+            tagForm.GetComponent<TagFormController>().SelectType(type);
+            selectModal.SetActive(false);
+        }
+
+    }
+
+}
