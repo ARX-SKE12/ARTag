5f92542372056fd3014df6a56bfa7ac31fbd76fe
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-04-19 21:43:51 +0700

    Vertical Image Tag

--- Assets/ARTag/Scripts/Tag/VerticalDescriptionImageTag.cs
+++ Assets/ARTag/Scripts/Tag/VerticalDescriptionImageTag.cs
@@ -1,16 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿
+namespace ARTag
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.UI;
 
-public class VerticalDescriptionImageTag : MonoBehaviour {
+    public class VerticalDescriptionImageTag : ImageCaptionTagBehaviour
+    {
+        public GameObject description;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        protected override JSONObject PrepareTagData(Dictionary<string, object> data)
+        {
+            JSONObject jsonData = base.PrepareTagData(data);
+            JSONObject detail = jsonData.GetField("detail");
+            detail.AddField("description", TextProcessor.ConvertFromNewLine(((string)data["description"])));
+            jsonData.SetField("detail", detail);
+            return jsonData;
+        }
+
+        protected override void ConstructTag(JSONObject data)
+        {
+            base.ConstructTag(data);
+            description.GetComponent<Text>().text = TextProcessor.ConvertToNewLine(data.GetField("tag").GetField("detail").GetField("description").str);
+        }
+    }
+
 }
