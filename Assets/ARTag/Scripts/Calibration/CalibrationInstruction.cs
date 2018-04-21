23d305a25e592fccfa739ce4ed1aa81d165c8189
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-04-21 15:16:30 +0700

    Refacetor Instruction Calibration

--- Assets/ARTag/Scripts/Calibration/CalibrationInstruction.cs
+++ Assets/ARTag/Scripts/Calibration/CalibrationInstruction.cs
@@ -2,39 +2,19 @@
 namespace ARTag
 {
     using UnityEngine;
-    using UnityEngine.UI;
-
-    public class CalibrationInstruction : MonoBehaviour
+    
+    public class CalibrationInstruction : Instruction
     {
-        public GameObject thumbnail, instructionText, changeStepButton, pointCloud, scanUI, calibrator;
-        public Sprite finishButtonSprite;
-        public Sprite[] stepThumbnail;
-        int step = 0;
+        public GameObject pointCloud, scanUI, calibrator;
 
-        public void NextStep()
+        protected override void FinishAction()
         {
-            switch (++step)
-            {
-                case 1:
-                    instructionText.GetComponent<Text>().text = "Feature Point is shown on the marker";
-                    thumbnail.GetComponent<Image>().sprite = stepThumbnail[0];
-                    break;
-                case 2:
-                    instructionText.GetComponent<Text>().text = "Tap On Center of Marker!";
-                    thumbnail.GetComponent<Image>().sprite = stepThumbnail[1];
-                    changeStepButton.GetComponent<Image>().sprite = finishButtonSprite;
-                    changeStepButton.GetComponentInChildren<Text>().text = "I got it!";
-                    break;
-                case 3:
-                    pointCloud.SetActive(true);
-                    scanUI.SetActive(true);
-                    calibrator.GetComponent<Calibration>().enabled = true;
-                    gameObject.SetActive(false);
-                    break;
-                default:
-                    break;
-            }
+            pointCloud.SetActive(false);
+            scanUI.SetActive(false);
+            calibrator.SetActive(false);
+            base.FinishAction();
         }
+        
     }
 
 }
---
+++ Assets/ARTag/Scripts/Instruction/Instruction.cs
@@ -0,0 +1,55 @@
+﻿
+namespace ARTag
+{
+    using UnityEngine;
+    using UnityEngine.UI;
+
+    public class Instruction : MonoBehaviour
+    {
+
+        public GameObject thumbnail, instructionText, changeStepButton;
+        public Sprite[] thumbnails;
+        public string[] guideTexts;
+        public Sprite finishButtonSprite;
+        public string finishButtonText = "I got it!";
+        public int numStep;
+
+        int step = 0;
+
+        // Use this for initialization
+        void Start()
+        {
+            NextStep();
+        }
+
+        public void NextStep()
+        {
+            UpdateInstruction();
+            step++;
+        }
+
+        void UpdateInstruction()
+        {
+            if (step < numStep)
+            {
+                thumbnail.GetComponent<Image>().sprite = thumbnails[step];
+                instructionText.GetComponent<Text>().text = guideTexts[step];
+                if (step == numStep-1)
+                {
+                    changeStepButton.GetComponent<Image>().sprite = finishButtonSprite;
+                    changeStepButton.GetComponentInChildren<Text>().text = finishButtonText;
+                }
+            } else
+            {
+                FinishAction();
+            }
+        }
+
+        protected virtual void FinishAction()
+        {
+            gameObject.SetActive(false);
+        }
+
+    }
+
+}
