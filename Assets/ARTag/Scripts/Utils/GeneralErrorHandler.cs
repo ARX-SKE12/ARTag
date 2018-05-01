1ebf317532e5bb67bcbb8b273196cc76e6e457b3
Voraton Lertrattanapaisal <voraton.l@ku.th>
2018-05-02 00:08:03 +0700

    Remove Place

--- Assets/ARTag/Scripts/Utils/GeneralErrorHandler.cs
+++ Assets/ARTag/Scripts/Utils/GeneralErrorHandler.cs
@@ -1,16 +1,32 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿
+namespace ARTag
+{
+    using UnityEngine;
+    using SocketIO;
+    using SocketIOManager;
+    using FBAuthKit;
 
-public class GeneralErrorHandler : MonoBehaviour {
+    public class GeneralErrorHandler : MonoBehaviour
+    {
+        SocketManager socketManager;
+        
+        // Use this for initialization
+        void Start()
+        {
+            socketManager.On(EventsCollector.AUTH_ERROR, OnError);
+            socketManager.On(EventsCollector.COLLABORATE_ERROR, OnError);
+            socketManager.On(EventsCollector.PLACE_CREATE_ERROR, OnError);
+            socketManager.On(EventsCollector.PLACE_ERROR_SIGNIFICANT, OnError);
+            socketManager.On(EventsCollector.PLACE_LIST_ERROR, OnError);
+            socketManager.On(EventsCollector.PLACE_UPDATE_ERROR, OnError);
+            socketManager.On(EventsCollector.PLANE_ERROR, OnError);
+            socketManager.On(EventsCollector.ROOM_ERROR, OnError);
+            socketManager.On(EventsCollector.TAG_ERROR, OnError);
+        }
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        public void OnError(SocketIOEvent e) {
+            if (e.data.GetField("error").str == "Token Lost") GameObject.FindObjectOfType<FacebookAuthController>().Auth();
+        }
+    }
+
 }
